a13b2305a84caf08a897b68de86d2768cc894288
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-01-19 14:36:27 +0100

    🔨 replace all require by import

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+	console.log("Your app is listening on port " + listener.address().port);
+});
