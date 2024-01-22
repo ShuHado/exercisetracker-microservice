376091fbe21d877bad104442ed07ab27b8122a7b
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-01-22 16:52:28 +0100

    🔨 cleanup code

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
-const app = express();
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/views/index.html");
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-	console.log("Your app is listening on port " + listener.address().port);
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
 });
